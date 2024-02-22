07cf25573ede9a77344f04a2deba91341ee90e24
Doctorspeppers <doctorspeppers@gmail.com>
2024-02-22 11:54:19 +0000

    fixing vite import

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -5,6 +5,7 @@
     <title>{{ $title ?? 'Pepper' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Fonts -->
+    <link href="/build/assets/app-BmB-b6nj.css" rel="stylesheet" />
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <!-- Styles -->
