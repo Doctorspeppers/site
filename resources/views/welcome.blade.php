8f098e3a75ec5095761ac148ca44417047ccf3a2
peper <peper@localhost.localdomain>
2024-06-20 16:15:08 -0300

    asset change in view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,7 @@
             <a href="https://ufsc.br/"
                 class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
                 <div class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
-                    <img class="size-12 items-center justify-center rounded-full" src="/storage/images/IFC.png">
+                    <img class="size-12 items-center justify-center rounded-full" src="{{ asset('/storage/images/IFC.png') }}">
                 </div>
 
                 <div class="pt-3 sm:pt-5">
@@ -70,7 +70,7 @@
                 class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
             >
                 <div class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
-                    <img class="size-12 items-center justify-center rounded-full" src="/storage/images/UFSC.png">
+                    <img class="size-12 items-center justify-center rounded-full" src="{{ asset("/storage/images/UFSC.png") }}">
                 </div>
 
                 <div class="pt-3 sm:pt-5">
@@ -104,7 +104,7 @@
         <a href="https://ehtl.com.br/"
             class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
             <div class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
-                <img class="size-12 items-center justify-center rounded-full" src="/storage/images/ehtl.jpeg">
+                <img class="size-12 items-center justify-center rounded-full" src="{{ asset('/storage/images/ehtl.jpeg') }}">
             </div>
 
             <div class="pt-3 sm:pt-5">
@@ -122,7 +122,7 @@
             class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
         >
             <div class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
-                <img class="size-12 items-center justify-center rounded-full" src="/storage/images/digitaleisso.jpeg">
+                <img class="size-12 items-center justify-center rounded-full" src="{{ asset('/storage/images/digitaleisso.jpeg') }}">
             </div>
 
             <div class="pt-3 sm:pt-5">
