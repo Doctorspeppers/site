1452b979c1e60034a2572c0f8fbff9992c9e60c3
peper <peper@localhost.localdomain>
2024-06-20 15:21:04 -0300

    Resolving PSR-4 Problems

--- app/View/Components/Curriculo/ItemResumo.php
+++ app/View/Components/Curriculo/ItemResumo.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class Resumo extends Component
+class ItemResumo extends Component
 {
     /**
      * Create a new component instance.
