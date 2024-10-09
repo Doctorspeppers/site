91dfc22499b7a4e2362703c92b31865ff112f005
Doctorspeppers <doctorspeppers@gmail.com>
2024-10-08 21:01:25 -0300

    Fix

--- resources/views/includes/teleprompter.blade.php
+++ resources/views/includes/teleprompter.blade.php
@@ -4,8 +4,6 @@
             @foreach (\App\Models\News::where('highlighted', 1)->orderBy('date')->paginate(50)->shuffle()->take(25) as $news)
                 <a href="{{ route('news.show', ['news'=>$news->id]) }}" class="truncate"> {{ str_replace(':', '', $news->title) }} </a>
                 <i class=" icon mt-1 icon-pepper-white ">
-                    <?xml version="1.0" encoding="iso-8859-1"?>
-<!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg fill="#F5F5F5" height="18px" width="18px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
 	 viewBox="0 0 454.534 454.534" xml:space="preserve">
